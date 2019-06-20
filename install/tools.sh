1453fc8e7bf6ff4c970d70c611738e89b6cf2a6b
qitas <qitas@qitas.cn>
2019-06-20 11:49:28 +0800

    fix add install

--- install/tools.sh
+++ install/tools.sh
@@ -3,6 +3,12 @@ shellPath=`pwd`
 
 sudo apt install -y gcc git wget make libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-cryptography python-future
 
-sudo echo "export PATH=$shellPath/../xtensa-esp32-elf/bin:$PATH" >> /etc/profile
-sudo echo "export PATH=$shellPath/../xtensa-lx106-elf/bin:$PATH" >> /etc/profile
+if [ -d  $shellPath/../xtensa-esp32-elf/bin ]; then
+    sudo echo 'export PATH="$shellPath/../xtensa-esp32-elf/bin:$PATH"' >> /etc/profile
+    source /etc/profile
+fi
+if [ -d  $shellPath/../xtensa-lx106-elf/bin ]; then
+    sudo echo 'export PATH="$shellPath/../xtensa-lx106-elf/bin:$PATH"' >> /etc/profile
+    source /etc/profile
+fi
 
