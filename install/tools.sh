7d91e60251afa33b92f2a5fde88bd02c84e80a0a
qitas <qitas@qitas.cn>
2019-06-20 16:29:27 +0800

    add shell

--- install/tools.sh
+++ install/tools.sh
@@ -2,10 +2,11 @@
 
 echo -e "ESP IDF CONFIG"
 shellPath=`pwd`
-
+cd $shellPath
 git clone -b v3.2 --recursive https://github.com/espressif/esp-idf.git
-cd ./esp-idf
+cd $shellPath/esp-idf
 git submodule update --init --recursive
 
 export IDF_PATH="$shellPath/esp-idf"
 python -m pip install --user -r $IDF_PATH/requirements.txt
+./add_path.sh
