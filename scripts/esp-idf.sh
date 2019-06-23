3f765df7605fe01bc7d51a7adab9746841e18f4f
qitas <qitas@qitas.cn>
2019-06-23 09:45:00 +0800

    fix add

--- scripts/esp-idf.sh
+++ scripts/esp-idf.sh
@@ -17,8 +17,8 @@ function set_esp_idf()
 	if [ -f $IDFPath/esp-idf/add_path.sh ]; then		
 		echo 'export IDF_PATH='$IDFPath'/esp-idf' >> ~/.bashrc
 		source ~/.bashrc
-		chmod +x $IDF_PATH/esp-idf/add_path.sh
-		$IDF_PATH/esp-idf/add_path.sh
+		chmod +x $IDF_PATH/add_path.sh
+		$IDF_PATH/add_path.sh
 		python -m pip install --user -r $IDF_PATH/requirements.txt
 		echo -e "done esp-idf path !\n${Line}"   	
 	fi	
